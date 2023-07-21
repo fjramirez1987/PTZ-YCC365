c9e5ddc824071808e6c9f791679199466247c830
Z <zanerv@protonmail.com>
2023-07-21 13:56:21 +0200

    jortan jt-8177

--- custom_components/ptz_camera/__init__.py
+++ custom_components/ptz_camera/__init__.py
@@ -6,32 +6,14 @@ DOMAIN = "ptz_camera"
 ATTR_HOST = "host"
 ATTR_PROFILE = "profile"
 DEFAULT_HOST = "192.168.1.244"
-DEFAULT_PROFILE = "Profile_1"
+DEFAULT_PROFILE = "PTZConfigToken"
 ATTR_PAN_TIME = "pan_time"
 DEFAULT_PANT_TIME = 10
 ATTR_MOVE_TIME = "move_time"
 DEFAULT_MOVE_TIME = 0.3
-DEFAULT_HEADERS = {'Content-Type':'application/soap+xml;charset=UTF8'}
+DEFAULT_HEADERS = {'Content-Type':'application/soap+xml'}
 
 def setup(hass, config):
-    
-    def stop(call):
-        host = call.data.get(ATTR_HOST, DEFAULT_HOST)
-        profile = call.data.get(ATTR_PROFILE, DEFAULT_PROFILE)
-
-        xml = """<?xml version="1.0" encoding="utf-8"?>
-        <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope"
-         xmlns:tptz="http://www.onvif.org/ver20/ptz/wsdl">
-         <soap:Body>
-         <tptz:Stop>
-         <tptz:ProfileToken>Profile_1</tptz:ProfileToken>
-         <tptz:PanTilt>true</tptz:PanTilt>
-         <tptz:Zoom>true</tptz:Zoom>
-         </tptz:Stop>
-         </soap:Body>
-        </soap:Envelope> """
-
-        r = requests.post('http://'+host+'/onvif/PTZ', data=xml, headers=DEFAULT_HEADERS)
 
     def move_left(call):
         host = call.data.get(ATTR_HOST, DEFAULT_HOST)
@@ -44,7 +26,7 @@ def setup(hass, config):
          xmlns:tt="http://www.onvif.org/ver10/schema">
          <soap:Body>
          <tptz:ContinuousMove>
-         <tptz:ProfileToken>Profile_1</tptz:ProfileToken>
+         <tptz:ProfileToken>PTZConfigToken</tptz:ProfileToken>
          <tptz:Velocity>
          <tt:PanTilt x="-0.5" y="0"/>
          <tt:Zoom x="1"/>
@@ -53,9 +35,8 @@ def setup(hass, config):
          </soap:Body>
         </soap:Envelope>"""
 
-        r = requests.post('http://'+host+'/onvif/PTZ', data=xml, headers=DEFAULT_HEADERS)
+        r = requests.post('http://'+host+':5000/onvif/ptz_service', data=xml, headers=DEFAULT_HEADERS)
         time.sleep(move_time)
-        stop(call)
 
     def move_right(call):
         host = call.data.get(ATTR_HOST, DEFAULT_HOST)
@@ -68,7 +49,7 @@ def setup(hass, config):
          xmlns:tt="http://www.onvif.org/ver10/schema">
          <soap:Body>
          <tptz:ContinuousMove>
-         <tptz:ProfileToken>Profile_1</tptz:ProfileToken>
+         <tptz:ProfileToken>PTZConfigToken</tptz:ProfileToken>
          <tptz:Velocity>
          <tt:PanTilt x="0.5" y="0"/>
          <tt:Zoom x="1"/>
@@ -77,10 +58,9 @@ def setup(hass, config):
          </soap:Body>
         </soap:Envelope>"""
 
-        r = requests.post('http://'+host+'/onvif/PTZ', data=xml, headers=DEFAULT_HEADERS)
+        r = requests.post('http://'+host+':5000/onvif/ptz_service', data=xml, headers=DEFAULT_HEADERS)
         time.sleep(move_time)
-        stop(call)
-   
+
     def move_up(call):
         host = call.data.get(ATTR_HOST, DEFAULT_HOST)
         profile = call.data.get(ATTR_PROFILE, DEFAULT_PROFILE)
@@ -92,7 +72,7 @@ def setup(hass, config):
          xmlns:tt="http://www.onvif.org/ver10/schema">
          <soap:Body>
          <tptz:ContinuousMove>
-         <tptz:ProfileToken>Profile_1</tptz:ProfileToken>
+         <tptz:ProfileToken>PTZConfigToken</tptz:ProfileToken>
          <tptz:Velocity>
          <tt:PanTilt x="0" y="0.5"/>
          <tt:Zoom x="1"/>
@@ -101,9 +81,8 @@ def setup(hass, config):
          </soap:Body>
         </soap:Envelope>"""
 
-        r = requests.post('http://'+host+'/onvif/PTZ', data=xml, headers=DEFAULT_HEADERS)
+        r = requests.post('http://'+host+':5000/onvif/ptz_service', data=xml, headers=DEFAULT_HEADERS)
         time.sleep(move_time)
-        stop(call)
 
     def move_down(call):
         host = call.data.get(ATTR_HOST, DEFAULT_HOST)
@@ -116,7 +95,7 @@ def setup(hass, config):
          xmlns:tt="http://www.onvif.org/ver10/schema">
          <soap:Body>
          <tptz:ContinuousMove>
-         <tptz:ProfileToken>Profile_1</tptz:ProfileToken>
+         <tptz:ProfileToken>PTZConfigToken</tptz:ProfileToken>
          <tptz:Velocity>
          <tt:PanTilt x="0" y="-0.5"/>
          <tt:Zoom x="1"/>
@@ -125,9 +104,8 @@ def setup(hass, config):
          </soap:Body>
         </soap:Envelope>"""
 
-        r = requests.post('http://'+host+'/onvif/PTZ', data=xml, headers=DEFAULT_HEADERS)
+        r = requests.post('http://'+host+':5000/onvif/ptz_service', data=xml, headers=DEFAULT_HEADERS)
         time.sleep(move_time)
-        stop(call)
 
     def move_origin_pan(call):
         host = call.data.get(ATTR_HOST, DEFAULT_HOST)
@@ -139,7 +117,7 @@ def setup(hass, config):
          xmlns:tt="http://www.onvif.org/ver10/schema">
          <soap:Body>
          <tptz:ContinuousMove>
-         <tptz:ProfileToken>Profile_1</tptz:ProfileToken>
+         <tptz:ProfileToken>PTZConfigToken</tptz:ProfileToken>
          <tptz:Velocity>
          <tt:PanTilt x="-0.5" y="0"/>
          <tt:Zoom x="1"/>
@@ -148,7 +126,7 @@ def setup(hass, config):
          </soap:Body>
         </soap:Envelope>"""
 
-        r = requests.post('http://'+host+'/onvif/PTZ', data=xml, headers=DEFAULT_HEADERS)
+        r = requests.post('http://'+host+':5000/onvif/ptz_service', data=xml, headers=DEFAULT_HEADERS)
 
     def move_origin_tilt(call):
         host = call.data.get(ATTR_HOST, DEFAULT_HOST)
@@ -160,7 +138,7 @@ def setup(hass, config):
          xmlns:tt="http://www.onvif.org/ver10/schema">
          <soap:Body>
          <tptz:ContinuousMove>
-         <tptz:ProfileToken>Profile_1</tptz:ProfileToken>
+         <tptz:ProfileToken>PTZConfigToken</tptz:ProfileToken>
          <tptz:Velocity>
          <tt:PanTilt x="0" y="-0.5"/>
          <tt:Zoom x="1"/>
@@ -169,7 +147,7 @@ def setup(hass, config):
          </soap:Body>
         </soap:Envelope>"""
 
-        r = requests.post('http://'+host+'/onvif/PTZ', data=xml, headers=DEFAULT_HEADERS)
+        r = requests.post('http://'+host+':5000/onvif/ptz_service', data=xml, headers=DEFAULT_HEADERS)
 
     def move_origin(call):
         pan_time = call.data.get(ATTR_PAN_TIME, DEFAULT_PANT_TIME)
@@ -182,7 +160,6 @@ def setup(hass, config):
     hass.services.register(DOMAIN, "move_right", move_right)
     hass.services.register(DOMAIN, "move_up", move_up)
     hass.services.register(DOMAIN, "move_down", move_down)
-    hass.services.register(DOMAIN, "stop", stop)
     hass.services.register(DOMAIN, "move_origin", move_origin)
     hass.services.register(DOMAIN, "move_origin_pan", move_origin_pan)
     hass.services.register(DOMAIN, "move_origin_tilt", move_origin_tilt)
